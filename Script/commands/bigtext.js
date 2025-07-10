a236e222dba1b7004efa1c8dc9f678436f4876db
justtry627 <justtry627@gmail.com>
2025-07-10 14:53:32 +0600

    Update bigtext.js

--- Script/commands/bigtext.js
+++ Script/commands/bigtext.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "bigtext",
     version: "1.0.0",
     hasPermssion: 0,
-    credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+    credits: "𝐂𝐘𝐁𝐄𝐑 _𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
     description: "big text",
     commandCategory: "game",
     usages: "bigtext <text>",
