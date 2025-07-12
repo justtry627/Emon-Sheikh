85f7b10b3f42346ae1338e6335c859904a29bd4c
justtry627 <justtry627@gmail.com>
2025-07-12 21:55:30 +0600

    Update groupadmin.js

--- Script/commands/groupadmin.js
+++ Script/commands/groupadmin.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "listadmin",
     version: '1.0.0',
     hasPermssion: 0,
-    credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+    credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
     description: "List of group administrators",
     commandCategory: "Box Chat",
     usages: "dsqtv",
