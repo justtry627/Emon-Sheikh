f4db9d1e9e4026c3eeb88a3f1470c719a8afaded
justtry627 <justtry627@gmail.com>
2025-07-13 12:33:18 +0600

    Update Rank.js

--- Script/commands/Rank.js
+++ Script/commands/Rank.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "rank",
 	version: "2.0.0",
 	hasPermssion: 0,
-	credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+	credits: "𝐂𝐘𝐁𝐄𝐑 _𖣘 -𝐁𝐎 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
 	description: "View Member Rankings",
 	commandCategory: "Group",
 	usages: " [user] or [tag]",
