c2bc37e0e2cabbb0163b186ab21a0c4a28fe5d9a
justtry627 <justtry627@gmail.com>
2025-07-14 07:55:00 +0600

    Update Trump.js

--- Script/commands/Trump.js
+++ Script/commands/Trump.js
@@ -4,7 +4,7 @@ module.exports.config = {
 
 	version: "1.0.1",
 	hasPermssion: 0,
-	credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+	credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
 	description: "Comment on the board ( ͡° ͜ʖ ͡°)",
 	commandCategory: "edit-img",
 	usages: "trump [text]",
