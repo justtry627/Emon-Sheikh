f6d2dd7502ce2b82ec74ff6f807e9e59e6a95085
justtry627 <justtry627@gmail.com>
2025-07-15 08:01:47 +0600

    Update outall.js

--- Script/commands/outall.js
+++ Script/commands/outall.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "outall",
 	version: "1.0.0",
 	hasPermssion: 2,
-	credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+	credits: "𝐂𝐘𝐁𝐄𝐑 _𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
 	description: "Send messages to groups!",
 	commandCategory: "Admin",
 	usages: "sendnoti [Text]",
