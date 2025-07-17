6912b2f6ccfbd7910cb5e1ca379bce5dd26e8c67
justtry627 <justtry627@gmail.com>
2025-07-17 23:29:43 +0600

    Update drake.js

--- Script/commands/drake.js
+++ Script/commands/drake.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "drake",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+  credits: "𝐂𝐘𝐁𝐄𝐑 _𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
   description: "drake",
   commandCategory: "Tiện ích",
   usages: "[text 1] | [text 2]",
